a4987ea0e10f88661eb2f4f8e9fef52fb429c6fe
RohanSubba <rohan.ronaldo07.rs@gmail.com>
2021-03-17 17:44:24 +0530

    task3 completed

--- app/src/main/java/com/example/recycleview/WorldListAdapter.java
+++ app/src/main/java/com/example/recycleview/WorldListAdapter.java
@@ -17,13 +17,29 @@ public class WorldListAdapter extends RecyclerView.Adapter<WorldListAdapter.Word
     private LayoutInflater mInflater;
 
 
-    class WordViewHolder extends RecyclerView.ViewHolder {
+    class WordViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
         public final TextView wordItemView;
         final WorldListAdapter mAdapter;
         public WordViewHolder(View itemView, WorldListAdapter adapter) {
             super(itemView);
             wordItemView = itemView.findViewById(R.id.word);
             this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // Get the position of the item that was clicked.
+            int mPosition = getLayoutPosition();
+            // Use that to access the affected item in mWordList.
+            String element = mWordList.get(mPosition);
+            // Change the word in the mWordList.
+            mWordList.set(mPosition, "Clicked! " + element);
+            // Notify the adapter, that the data has changed so it can
+            // update the RecyclerView to display the data.
+            mAdapter.notifyDataSetChanged();
+
         }
     }
     public WorldListAdapter(Context context,
